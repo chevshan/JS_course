78e40a0798b5b0d461dae40f29d6c8bcb92e11e6
Illia <ilmal2004@mail.ru>
2024-03-10 21:06:18 +0300

    create first project

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-"use sctrict";
+"use strict";
 
-const answer = prompt("Вам есть 18?", "")
-console.log(answer)
+const numberOfFilms = prompt("Скок посмотрел? ;)", "да");
+console.log(numberOfFilms);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const lastMovie = prompt("Какой фильм последний Вы смотрели?", "");
+const lastMovieRating = prompt("И как Вам?", "");
+
+const movies = {
+    lastMovie: {}
+}
+movies.lastMovie = lastMovieRating;
+
+console.log(movies.lastMovie);
